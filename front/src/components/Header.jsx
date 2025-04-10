a851093d89bf66fd7265c79f6e74e5f6ac85ac94
BrunoPdOliveir4 <bpedrosodroliveira10@gmail.com>
2025-04-10 00:00:57 -0300

    fix main page

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -7,11 +7,11 @@ function Header(){
         <div id="headerbg">
             <header>
                 <img src="./src/assets/Logoext.png" alt="Logo"/>
-                
                 <div>
-                    <button id="login" onClick={() =>{navigate('/login')}}>Entrar</button>
-                    <button id="signin" onClick={() => navigate('/cadastro')}>Registrar</button>
+                    <h1 style={{ color: 'white', fontSize: '1.5rem',padding: '0 1em' }}>O melhor Streaming de Animes da América Latina!</h1>
                 </div>
+                    <button onClick={() =>{navigate('/login')}}>Entrar</button>
+                
             </header>
         </div>
     )
--- front/src/components/InviteSub.jsx
+++ front/src/components/InviteSub.jsx
@@ -8,10 +8,11 @@ export const InviteSub = () => {
 
             <div className="invite-sub__container">
                 <div style={{ display: 'flex', flexDirection: 'column', color: 'white' }}>
-                    <h1>O melhor Streaming de Animes da América Latina!</h1>
+                    <h1>Você não irá se arrepender de participar de nossa comunidade!</h1>
+                    <h2>Registre-se e participe conosco</h2>
                 </div>
             </div>  
-            <div className="card" style={{ display: 'flex', gap: '1rem', alignItems: 'center' }}>
+            <div className="card" style={{ display: 'flex', gap: '1rem', alignItems: 'start' }}>
                 <input type="text" name="" id="myEmail" placeholder='Digite seu Email' />
                 <button id="invite" onClick={() => navigate('/cadastro')}>
                 Vamos lá!
